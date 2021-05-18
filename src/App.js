59f32fed1ceca8f7c5062583a09aae1422343cc3
Andrew Aiello <andrewaiello7@gmail.com>
2021-05-18 22:18:27 +1000

    splash screen doesn't appear anymore when database value changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import MapContainer from './pages/MapContainer/MapContainer';
 import Register from './pages/Register/Register';
 import SignIn from './pages/SignIn/SignIn';
@@ -7,12 +7,41 @@ import AddToHomescreen from 'react-add-to-homescreen';
 import './App.css';
 import { handleAddToHomeScreen } from './functions/handleAddToHomeScreen';
 import Logo from "./assets/images/logo192.png";
+import { pins } from './Pins/Pins'
+import { db } from './services/firebase'
 
 function App() {
-
   const [route, setRoute] = useState('start')
-
   const onRouteChange = (newRoute) => setRoute(newRoute) 
+  const [value1, setValue1] = useState(0)
+
+  useEffect(() => {
+    const getValue = db.ref("sensor1");
+    getValue.on("value", snapshot => {
+      let value = snapshot.val();
+      if(value == 1 && value1 == 0){
+        pins[0] = {
+          lat: -33.7971,
+            lng: 151.1836,
+            type: 'Spot Taken',
+            hours: 'N/A',
+            days: 'N/A',
+            address: '1 Anderson St, Chatswood NSW 2067',
+      }
+      }
+      else if(value == 0 && value1 == 1){
+        pins[0] = {
+          lat: -33.7971,
+            lng: 151.1836,
+            type: 'Free Parking',
+            hours: 'All day',
+            days: 'Every day',
+            address: '1 Anderson St, Chatswood NSW 2067',
+      }
+      }
+      setValue1(value)
+    });
+  }, []);
 
   const loadPage = () => {
     switch(route) {
@@ -23,7 +52,7 @@ function App() {
       case 'signin':
         return <SignIn onRouteChange={onRouteChange} />
       case 'home':
-        return <MapContainer onRouteChange={onRouteChange} />
+        return <MapContainer onRouteChange={onRouteChange} value1={value1} />
       default:
         return <>Invalid route</>
     }
@@ -35,8 +64,8 @@ function App() {
         {
           loadPage()
         }
-        <AddToHomescreen icon={Logo} onAddToHomescreenClick={handleAddToHomeScreen} />
-    </div>
+        <AddToHomescreen icon={Logo} onAddToHomescreenClick={handleAddToHomeScreen} />    
+    </div>    
   );
 }
 
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,14 @@
 import firebase from 'firebase';
 
+
+const firebaseProjectName = "TestFirebase1";
+
 const config = {
-    apiKey: 'AIzaSyCiRHh7hmFCKanlFR64NNUJYVR26GQS32U',
-    authDomain: "parking-sensors-database.firebaseapp.com",
-    databaseURL: "https://parking-sensors-database-default-rtdb.firebaseio.com"
-  };
+  apiKey: "AIzaSyAyb8LrUGZbzHPGQ1ItQ3sAji7bqwu3Bqg",
+  authDomain: "test-5a81d.firebaseapp.com",
+  databaseURL: "https://test-5a81d-default-rtdb.firebaseio.com",
+  projectId: "test-5a81d"
+};
   firebase.initializeApp(config);
   export const auth = firebase.auth;
   export const db = firebase.database();
